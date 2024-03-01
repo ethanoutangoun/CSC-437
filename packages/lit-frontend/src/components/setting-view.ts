4aa3a45131482e7f0b5dc4bf34509c526775d76f
ethanoutangoun <ethanoutangoun@gmail.com>
2024-03-01 11:16:45 -0800

    fix typescript warnings for deployment

--- packages/lit-frontend/src/components/setting-view.ts
+++ packages/lit-frontend/src/components/setting-view.ts
@@ -1,4 +1,4 @@
-import { css, html, LitElement, unsafeCSS } from "lit";
+import { css, html, LitElement } from "lit";
 import { customElement } from "lit/decorators.js";
 
 @customElement("setting-view")
